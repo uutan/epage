5362a99d241bc43fc1085207bf5ff8749bbb060d
杨谈芳 <uutan@qq.com>
2021-01-23 16:52:34 +0800

    remove hook text

--- src/panel/setting/index.vue
+++ src/panel/setting/index.vue
@@ -1,39 +1,27 @@
 <template lang="pug">
-Tabs(:value='tab' size='small')
-  a.ep-setting-help-link(slot="extra" :href='docURL' target='_blank')
-    Icon(type="help")
-    span &nbsp;帮助
-
-  TabPane(label='属性' name='prop')
+Tabs(:value='tab', size='small')
+  TabPane(label='属性', name='prop')
     ep-panel.ep-setting-prop
       template(v-if='isSelected && settingWidget')
-        component(:is='settingWidget' :store='store' :setting='setting')
+        component(:is='settingWidget', :store='store', :setting='setting')
 
-  TabPane(
-    label='样式'
-    name='style'
-    v-if='isShowSetting("style")'
-  )
+  TabPane(label='样式', name='style', v-if='isShowSetting("style")')
     ep-panel.ep-setting-form
       ep-style-setting(
-        v-if='isSelected'
-        :store='store'
-        :schema='selectedSchema'
+        v-if='isSelected',
+        :store='store',
+        :schema='selectedSchema',
         :root='false'
       )
 
-  TabPane(
-    label='页面'
-    name='global'
-    v-if='isShowSetting("global")'
-  )
+  TabPane(label='页面', name='global', v-if='isShowSetting("global")')
     ep-panel.ep-setting-form
       form-setting(:store='store')
 
   TabPane(
-    v-for='(s, index) in getFilterSettings()'
-    :label='s.title'
-    :key='s.key'
+    v-for='(s, index) in getFilterSettings()',
+    :label='s.title',
+    :key='s.key',
     :name='s.key'
   )
     ep-panel.ep-setting-form
@@ -104,14 +92,16 @@ export default {
   },
   methods: {
     isShowSetting (name) {
-      return this.settings.filter(s => s.key === name).length === 0
+      return this.settings.filter((s) => s.key === name).length === 0
     },
     getFilterSettings () {
-      return this.settings.filter(setting => {
-        return setting &&
-        setting.component &&
-        typeof setting.key === 'string' &&
-        !!setting.key
+      return this.settings.filter((setting) => {
+        return (
+          setting &&
+          setting.component &&
+          typeof setting.key === 'string' &&
+          !!setting.key
+        )
       })
     }
   }
